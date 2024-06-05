3cacba00f20ec74407e55bf3e6177657fac6fc29
Sourabha K H <sourabhaprasad04@gmail.com>
2024-06-06 01:56:45 +0530

    queries

--- 03-joining-data/countries-dataset/countries.sql
+++ 03-joining-data/countries-dataset/countries.sql
@@ -76,3 +76,117 @@ ON c.code = p.country_code
 INNER JOIN economies AS e
 ON c.code = e.code
 	AND p.year = e.year;
+
+
+-- LEFT JOIN
+SELECT c.name as country, local_name, l.name as language, percent
+FROM countries AS c
+LEFT JOIN languages AS l
+USING (code)
+ORDER BY country DESC;
+
+
+SELECT 
+    c1.name AS city,
+    code,
+    c2.name AS country,
+    region,
+    city_proper_pop
+FROM cities AS c1
+INNER JOIN countries AS c2
+ON c1.country_code = c2.code
+ORDER BY code DESC;
+-- Result of 230 rows using INNER JOIN
+
+
+SELECT 
+	c1.name AS city, 
+    code, 
+    c2.name AS country,
+    region, 
+    city_proper_pop
+FROM cities AS c1
+LEFT JOIN countries as c2
+ON c1.country_code = c2.code
+ORDER BY code DESC;
+-- Result of 236 rows using LEFT JOIN
+-- Remember that the LEFT JOIN is a type of outer join: its result is not limited to only those records that have matches for both tables on the joining field.
+
+SELECT name, region, gdp_percapita
+FROM countries AS c
+LEFT JOIN economies AS e
+USING (code)
+WHERE year = 2010;
+
+
+SELECT region, AVG(gdp_percapita) AS avg_gdp
+FROM countries AS c
+LEFT JOIN economies AS e
+USING(code)
+WHERE year = 2010
+GROUP BY region
+ORDER BY avg_gdp DESC
+LIMIT 10;
+
+
+--
+-- Modify this query to use RIGHT JOIN instead of LEFT JOIN
+SELECT countries.name AS country, languages.name AS language, percent
+FROM languages
+RIGHT JOIN countries
+USING(code)
+ORDER BY language;
+
+-- Modify this query to use RIGHT JOIN instead of LEFT JOIN
+SELECT countries.name AS country, languages.name AS language, percent
+FROM languages
+RIGHT JOIN countries
+USING(code)
+ORDER BY language;
+-- when converting a LEFT JOIN to a RIGHT JOIN, change both the type of join and the order of the tables to get equivalent results. You would get different results if you only changed the table order. The order of fields you are joining ON still does not matter.
+
+-- FULL JOIN
+SELECT name AS country, code, region, basic_unit
+FROM countries
+FULL JOIN currencies
+USING (code)
+WHERE region = 'North America'
+    OR name IS NULL
+ORDER BY region;
+
+
+-- LEFT JOIN
+SELECT name AS country, code, region, basic_unit
+FROM countries
+LEFT JOIN currencies
+USING (code)
+WHERE region = 'North America' 
+	OR name IS NULL
+ORDER BY region;
+
+
+-- INNER JOIN
+SELECT name AS country, code, region, basic_unit
+FROM countries
+INNER JOIN currencies 
+USING (code)
+WHERE region = 'North America' 
+	OR name IS NULL
+ORDER BY region;
+
+-- NOTE:  The FULL JOIN query returned 18 records, the LEFT JOIN returned four records, and the INNER JOIN only returned three records
+
+
+SELECT 
+	c1.name AS country, 
+    region, 
+    l.name AS language,
+	basic_unit, 
+    frac_unit
+FROM countries AS c1 
+FULL JOIN languages AS l
+USING(code)
+FULL JOIN currencies AS c2
+USING(code)
+WHERE region LIKE 'M%esia';
+-- The first FULL JOIN in the query pulled countries and languages, and the second FULL JOIN added in currency data for each record in the result of the first FULL JOIN
--- 03-joining-data/leaders-database/leaders.sql
+++ 03-joining-data/leaders-database/leaders.sql
@@ -29,4 +29,36 @@ ON pm.prime_minister = pmt.name;
 
 
 SELECT *
-FROM public.prime_minister_terms;
+FROM prime_ministers;
+
+SELECT *
+FROM presidents;
+
+-- OUTER JOIN
+-- LEFT JOIN
+-- To include all countries with prime ministers, presidents if exists else missing values/ NULL
+SELECT country, prime_minister, president
+FROM prime_ministers AS pm
+LEFT JOIN presidents AS p
+USING (country);
+
+
+SELECT *
+FROM prime_ministers;
+
+SELECT *
+FROM presidents;
+
+-- RIGHT JOIN : with presidents & prime ministers
+SELECT p.country, prime_minister, president
+FROM prime_ministers AS pm
+RIGHT JOIN presidents as p
+USING (country);
+
+
+-- FULL JOIN
+SELECT COALESCE(pm.country, p.country) AS country, prime_minister, president
+FROM prime_ministers as pm 
+FULL JOIN presidents as p
+ON pm.country = p.country;
+
